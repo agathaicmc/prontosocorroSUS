97b84a9c83bc9b00f20ce776ffedc5ee31930d69
agathaicmc <aregis2511@gmail.com>
2025-08-26 13:19:51 -0300

    adicionei o historico.hpp e o historico.cpp com implementação da maioria das funções da pilha. falta o destrutor e a consulta, além de corrigir o retorno da ufnção retirar

---
+++ historico.cpp
@@ -0,0 +1,78 @@
+#include <cstdio>
+#include "historico.hpp"
+
+/*
+construtor da pilha
+*/
+
+historico::historico(){
+    topo = nullptr;
+    qtd = 0;
+}
+
+/*
+destrutor da pilha
+*/
+
+historico::~historico(){
+
+}
+
+// função que insere um novo procedimento no histórico do paciente
+bool historico::inserir(string str){
+    // checa se o historico do paciente está cheio
+    if(historicoCheio()){
+        printf("Não é possível adicionar mais procedimentos no histórico deste paciente.\n");
+        return false;
+    }
+    
+    // cria um novo procedimento
+    procedimento *aux = new procedimento;
+    // coloca o dado dentro do procedimento
+    aux->tratamento = str;
+    // agora o prox aponta para o topo antigo
+    aux->prox = topo;
+    // atualiza o topo para apontar para aux, o novo topo
+    topo = aux;
+    // atualiza a quantidade de procedimentos no histórico
+    qtd++;
+
+    return true;
+}
+
+
+string historico::retirar(){
+    if(historicoVazio()){
+        printf("Não há procedimentos para serem retirados do histórico deste paciente.\n");
+        return;
+    }
+    // guarda o procedimento atual em um elemento
+    string elemento = topo->tratamento;
+    // cria um auxiliar que aponta para o topo
+    procedimento *aux = topo;
+    // atualiza o topo para apontar para o elemento abaixo dele
+    topo = topo->prox;
+    // deleta o topo antigo
+    delete aux;
+    // atualiza a quantidade de elementos no histórico
+    qtd--;
+
+    return elemento;
+}
+
+void historico::consultar(){
+
+}
+
+// função que checa se o historico do paciente está cheio
+bool historico::historicoCheio(){
+    if (qtd == 10) return true;
+    else return false;
+}
+
+
+// função que checa se o historico do paciente está vazio
+bool historico::historicoVazio(){
+    if (qtd == 0) return true;
+    else return false;
+}
